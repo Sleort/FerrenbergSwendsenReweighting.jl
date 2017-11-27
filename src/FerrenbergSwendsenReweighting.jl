64aa11624cfb59f1a1ba8a7243d9307419ac8ad4
Unknown <tr-ab@online.no>
2017-11-27 19:04:51 +0900

    In-place as well as out-of-place weight calculation...

--- src/FerrenbergSwendsenReweighting.jl
+++ src/FerrenbergSwendsenReweighting.jl
@@ -30,45 +30,46 @@ struct Reweights{T1,T2}
     logprob::Function #exp(logprob(λ,x[i])) is the probability weight of sample x[i]
     δlogprob::T1 #Vector of shifts in logprob
     x::T2 #Input values/samples
-    w::T1 #Probability weight storage (normalized!)
 
-    function Reweights{T1,T2}(logprob::Function, δlogprob::T1, x::T2, w::T1) where {T1<:AbstractVector, T2}
-        @argcheck length(δlogprob) == length(x) == length(w)
-        new(logprob, δlogprob, x, w)
+    function Reweights{T1,T2}(logprob::Function, δlogprob::T1, x::T2) where {T1<:AbstractVector, T2}
+        @argcheck length(δlogprob) == length(x)
+        new(logprob, δlogprob, x)
     end
 end
 
 
-function Reweights{T}(logprob::Function, δlogprob::T1, x::T2, w::T1) where {T<:Real, T1<:AbstractVector{T}, T2}
-    Reweights{T1,T2}(logprob, δlogprob, x, w)
-end
 
-
-#Default type is Float64!
-Reweights(args...; kwargs...) = Reweights{Float64}(args...; kwargs...)
-
-
-"Default reweighting to Boltzmann distribution, i.e. assuming x0=E, λ=-β"
-boltzmann(β,E) = -β*E
-Reweights{T}(args...; kwargs...) where T = Reweights{T}(boltzmann, args...; kwargs...)
-
-
-"""Return the "reweights" at parameter λ"""
-function (rw::Reweights)(λ)
+"""Return the "Reweights" at parameter λ"""
+#In place calculation HOW TO MAKE THIS NICE LOOKING WITH EXCLAMATION MARK??
+function (rw::Reweights{T1,T2})(w::T1, λ) where {T1,T2}
+    @argcheck length(rw.x) == length(w)
     for (i,xi) ∈ enumerate(rw.x)
-        rw.w[i] = rw.logprob(λ, xi) + rw.δlogprob[i]
+        w[i] = rw.logprob(λ, xi) + rw.δlogprob[i]
     end
-    mw = maximum(rw.w)
-    rw.w .= exp.(rw.w .- mw) #Make sure the weights are maximum 1 (avoid overflow)
-    normalize!(rw.w, 1) #sum(w) = 1
-    return rw.w
+    mw = maximum(w)
+    w .= exp.(w .- mw) #Make sure the weights are maximum 1 (avoid overflow)
+    normalize!(w, 1) #1-norm, such that sum(w) = 1
+    return w
+end
+
+#Out-of place:
+function (rw::Reweights)(λ)
+    w = similar(rw.δlogprob)
+    rw(w, λ)
 end
 
 
 ##################################
 include("SingleHistogramReweighting.jl")
 include("MultipleHistogramReweighting.jl")
-##################################
 
+
+##################################
+#Default type is Float64!
+Reweights(args...; kwargs...) = Reweights{Float64}(args...; kwargs...)
+
+"Default reweighting to Boltzmann distribution, i.e. assuming x0=E, λ=-β"
+boltzmann(β,E) = -β*E
+Reweights{T}(args...; kwargs...) where T = Reweights{T}(boltzmann, args...; kwargs...)
 
 end # module
