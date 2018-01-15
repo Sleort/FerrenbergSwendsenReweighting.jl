21896886f5e18d0568febe371162909eeac46046
Unknown <tr-ab@online.no>
2018-01-15 18:52:55 +0900

    Updates
    * The ReweightObj is now also parametrized by the type of the logprob function.
    * Now using MCMCDiagnostics.jl
    * Improved numerical stability by adding some (tiny) noise to the data from which the effective sample size factor is obtained.

--- src/SingleHistogramReweighting.jl
+++ src/SingleHistogramReweighting.jl
@@ -1,7 +1,9 @@
 "Single histogram reweighting object"
 function ReweightObj{T}(logprob::Function, λ0, x::AbstractVector) where T<:Real
     δlogprob = T[-logprob(λ0, xi) for xi ∈ x]
+
+    F = typeof(logprob)
     T1 = typeof(δlogprob)
     T2 = typeof(x)
-    return ReweightObj{T1,T2}(logprob, δlogprob, x)
+    return ReweightObj{F,T1,T2}(logprob, δlogprob, x)
 end
